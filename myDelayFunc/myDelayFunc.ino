f941907ee4dae3c14210e6f3fdfd020d33b07998
Vitor Mayer <vpmayer@hotmail.com>
2020-05-28 12:55:53 -0300

    fix text

--- myDelayFunc/myDelayFunc.ino
+++ myDelayFunc/myDelayFunc.ino
@@ -3,6 +3,7 @@ void setup() {
   PORTB = 0b00100000;
 }
 const uint32_t delayValue = 200;
+
 void myDelay(uint32_t ms);
 
 void loop() {
@@ -17,12 +18,14 @@ void loop() {
 }
 
 void myDelay(uint32_t ms){
-  //F_CPU contem a informação da frequancia de cloque principal do uC
-  //esse for consome 7 instruções de maquina no assembler
-  //a instrução demora 1 ciclo de maquina, sondiderando F_CPU = 16000000
-  // o loop roda a F_CPU/7 Hz = 2285714,2857 
-  // 1 ms = 1/1000 Hz
-  // portanto  a cada 2285,7142857 instruções ele consome 1 ms
+  /*
+    F_CPU contém a informação da frequancia de clock principal do uC
+    esse for consome 7 instruções de máquina no assembler
+    a instrução demora 1 ciclo de máquina, sondiderando F_CPU = 16000000
+    o loop roda a F_CPU/7 Hz = 2285714,2857 
+    1 ms = 1/1000 Hz
+    portanto  a cada 2285,7142857 instruções ele consome 1 ms
+  */
   const uint32_t instMs = F_CPU/7000;
   uint32_t i;
 
