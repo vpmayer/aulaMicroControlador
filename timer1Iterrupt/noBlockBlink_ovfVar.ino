025e5fe51202c81347b37afe038ba8a4bfd4e147
Vitor Mayer <vpmayer@hotmail.com>
2020-06-02 15:51:34 -0300

    add proggram to pre load value on TCNT1 to configure overflow time

---
+++ timer1Iterrupt/noBlockBlink_ovfVar.ino
@@ -0,0 +1,49 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define BTO 7
+#define LED_B 12
+#define LED_G 11
+
+#define TimerLoader = 32768
+
+
+void setup() {
+  pinMode(LED_B, OUTPUT);  
+  pinMode(LED_G, OUTPUT);  
+  pinMode(BTO, INPUT_PULLUP);
+
+  cli(); // disable global interrupts
+
+  TCCR1A = 0; // clear TCCR1A
+  TCCR1B = 0; // clear TCCR1B
+
+  TIMSK1 = (1 << TOIE1); // enable overflow interrupt
+  TCCR1B |= (1 << CS12); // set prescale to F_CPU/256
+
+  TCNT1 = 32768; // pre load timer counter
+
+  sei(); // enable global interrupts
+}
+
+bool status_led = false;
+bool status_led_g = false;
+
+void loop() {
+  	
+  digitalWrite(LED_B, status_led);   
+
+	status_led = !status_led;
+
+	while(!digitalRead(BTO)){
+		delay(100);
+	}
+
+	delay(500);
+}
+
+ISR(TIMER1_OVF_vect) {
+	status_led_g = !status_led_g;
+	digitalWrite(LED_G,status_led_g);
+	TCNT1 = 32768; 
+}
