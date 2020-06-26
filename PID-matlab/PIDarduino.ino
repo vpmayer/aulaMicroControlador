6cbb66bd83602b08cfea2d27c06e54583359b9e6
Vitor Mayer <vpmayer@hotmail.com>
2020-06-26 09:00:46 -0300

    adjust Compute function

--- PID-matlab/PIDarduino.ino
+++ PID-matlab/PIDarduino.ino
@@ -15,9 +15,9 @@ int processVariable = 0;
 unsigned long lastTime;
 double Input, Output, Setpoint;
 double errSum, lastErr;
-double kp=4.0;
-double ki=0.001;
-double kd=0;
+double kp=4.0F;
+double ki=1.0F;
+double kd=0.001F;
 
 void setup() {
   Serial.begin(115200);
@@ -55,14 +55,16 @@ void loop() {
     }
 
   }
-  processVariable = map(analogRead(PV_PIN),0,1023,0,100);
+  processVariable = map(analogRead(PV_PIN),0,1023,0,1000);
   Input = processVariable;
 
   // manipulated = map(setpoint,0,100,0,255);
+  
+  msgPrint(setpoint, Output, processVariable);
 
+  Output = map(Output,0,1000,0,255);
   Output = constrain(Output,0,255);
   analogWrite(OUTPUT_PIN, Output);
-  msgPrint(setpoint, Output, processVariable);
   Compute();
   delay(100);
 }
@@ -86,11 +88,13 @@ void msgPrint(int setpoint, int manipulated,int PV){
 
 void Compute(){
   unsigned long now = millis();
-  double timeChange = (double)(now - lastTime);
-  double error = Setpoint - Input;
-  errSum += (error * timeChange);
-  double dErr = (error - lastErr) / timeChange;
-  Output = kp * error + ki * errSum + kd * dErr;
-  lastErr = error;
-  lastTime = now;
+  double timeChange = (double)(now - lastTime); //time interval
+  timeChange /= 1000.0F;
+  double error = Setpoint - Input; //erro 
+  errSum += (error * timeChange); // somatoria do erro (integral do erro)
+  double dErr = (error - lastErr) / timeChange; //diferença do erro (derivada do erro)  
+  Output = kp * error + ki * errSum + kd * dErr; 
+  // Saida = ganho Direto + ganho itegral * integral do erro + ganho derivada + derivada do erro
+  lastErr = error; //atualiza o erro
+  lastTime = now; //atualiza o ultimo tempo
 }
